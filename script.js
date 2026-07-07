ff81dd8ee512b7911a66b72ab4db9fe1f0969c49
Mohammad Shahnawaz Faiyaz <faiyazmohammadshahnawaz@gmail.com>
2026-07-07 18:25:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,92 @@
-const quotes = [
+const quotes=[
+
 {
 quote:"The future depends on what you do today.",
 author:"Mahatma Gandhi"
 },
+
 {
 quote:"Success is not final. Failure is not fatal.",
 author:"Winston Churchill"
 },
+
 {
 quote:"Dream big and dare to fail.",
 author:"Norman Vaughan"
 },
+
 {
 quote:"Believe you can and you're halfway there.",
 author:"Theodore Roosevelt"
 },
+
 {
 quote:"Stay hungry. Stay foolish.",
 author:"Steve Jobs"
+},
+
+{
+quote:"Education is the passport to the future.",
+author:"Malcolm X"
+},
+
+{
+quote:"Knowledge is power.",
+author:"Francis Bacon"
+},
+
+{
+quote:"Never stop learning.",
+author:"Unknown"
 }
+
 ];
+
+let currentQuote={};
 
 function newQuote(){
 
-const random = Math.floor(Math.random()*quotes.length);
+const random=Math.floor(Math.random()*quotes.length);
 
-document.getElementById("quote").innerHTML =
-quotes[random].quote;
+currentQuote=quotes[random];
 
-document.getElementById("author").innerHTML =
-"- " + quotes[random].author;
+document.getElementById("quote").innerHTML=currentQuote.quote;
+
+document.getElementById("author").innerHTML="- "+currentQuote.author;
+
+}
+
+function copyQuote(){
+
+const text=currentQuote.quote+" - "+currentQuote.author;
+
+navigator.clipboard.writeText(text);
+
+alert("Quote copied successfully!");
+
+}
+
+function shareQuote(){
+
+const text=currentQuote.quote+" - "+currentQuote.author;
+
+if(navigator.share){
+
+navigator.share({
+
+title:"Random Quote",
+
+text:text
+
+});
+
+}
+
+else{
+
+alert("Sharing is not supported on your browser.");
+
+}
 
 }
 
